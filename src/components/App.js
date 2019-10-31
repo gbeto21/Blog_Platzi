e8207fd2cbfb3b58716e0c82f8af1e4de17cc57e
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-31 13:28:08 -0600

    Se creó el proyecto y la tabla de la pagina principal. Se terminó la clase: 'Creación de la app con React.'

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='margen'>
+
+      <table className='tabla'>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Rodolfo
+        </td>
+            <td>
+              Rodolfo@saldivar.com
+        </td>
+            <td>
+              Rodolfo.com
+        </td>
+          </tr>
+          <tr>
+            <td>
+              Platzi
+        </td>
+            <td>
+              Platzi@saldivar.com
+        </td>
+            <td>
+              Platzi.com
+        </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  )
+}
+
+export default App
