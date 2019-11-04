d578f313a9037b506d84ca0294a68e9d0d672423
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-04 12:36:32 -0600

    Se dotó al action index para poder realizar la llamada para obtener los usuarios. Se terminó la clase: 'Redux Thunk'.

--- src/components/usuarios/index.js
+++ src/components/usuarios/index.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { connect } from 'react-redux'
 import * as usuariosActions from '../../actions/usuariosActions'
 
 class Usuarios extends Component {
 
   componentDidMount() {
-
-    // const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
-
-    // this.setState({
-    //   usuarios: respuesta.data
-    // })
 
     this.props.traerTodos()
 
@@ -34,9 +27,6 @@ class Usuarios extends Component {
   )
 
   render() {
-
-    console.log(this.props)
-
     return (
       <div>
 
