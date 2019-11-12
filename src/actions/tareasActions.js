8615e7b55f192bef920d2fdb2f03a2302fd9970e
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-11 21:30:28 -0600

    Se realizó la modificación para obtener las tareas de las publicaciones seleccionadas. Se terminó la clase: 'Normalizar datos'.

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -10,9 +10,21 @@ export const traerTodas = () => async (dispatch) => {
     try {
         const respuesta = await axios.get('https://jsonplaceholder.typicode.com/todos')
 
+        const tareas = {}
+
+        respuesta.data.map((tar) => (
+            tareas[tar.userId] = {
+                ...tareas[tar.userId],
+                [tar.id]:
+                {
+                    ...tar
+                }
+            }
+        ))
+
         dispatch({
             type: TRAER_TODAS,
-            payload: respuesta.data
+            payload: tareas
         })
     }
     catch (error) {
