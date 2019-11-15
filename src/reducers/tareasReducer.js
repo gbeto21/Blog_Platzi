ab9e064fdb59364268b546d3363b2290282bfee5
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-14 19:20:19 -0600

    Se agregó la lógica para mantener checadas las tareas que se seleccionaron. Se terminó la clase: 'PUT'.

--- src/reducers/tareasReducer.js
+++ src/reducers/tareasReducer.js
@@ -4,7 +4,8 @@ import {
     ERROR,
     CAMBIO_USUARIO_ID,
     CAMBIO_TITULO,
-    AGREGADA
+    GUARDAR,
+    ACTUALIZAR
 } from '../types/tareasTypes'
 import { bindActionCreators } from "redux";
 
@@ -40,7 +41,7 @@ export default (state = INITIAL_STATE, action) => {
         case CAMBIO_TITULO:
             return { ...state, titulo: action.payload }
 
-        case AGREGADA:
+        case GUARDAR:
             return {
                 ...state,
                 tareas: {},
@@ -50,6 +51,9 @@ export default (state = INITIAL_STATE, action) => {
                 usuario_id: '',
                 titulo: ''
             }
+
+        case ACTUALIZAR:
+            return { ...state, tareas: action.payload }
 
         default: return state;
     }
